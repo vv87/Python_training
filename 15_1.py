3c1e45a48d1f6b364add250c5a03f08251a30d36
vv87 <38062151+vv87@users.noreply.github.com>
2019-07-10 17:06:45 +0300

    Create 15_1.py

---
+++ 15_1.py
@@ -0,0 +1,17 @@
+assert abs(-42) == 42, "Should be absolute value of a number"
+
+name = "Grace"
+print('Hello, {}\n'.format(name))
+
+
+a = 5
+b = 10
+
+print((f'Five plus ten is {a + b} and not {2 * (a + b)}.'
+       'Five plus ten is 15 and not 30.')
+      )
+
+# name = "Eric"
+age = 74
+
+print(f"\nHello, {name}. You are {age}.")
