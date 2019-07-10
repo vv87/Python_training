5d240a09a1ceaf4986f3289ae1895a744c5dd110
vv87 <38062151+vv87@users.noreply.github.com>
2019-07-10 17:08:44 +0300

    Create 18.py

---
+++ 18.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import time
+import pytest
+import math
+
+
+@pytest.mark.parametrize('links', ['236895/step/1', '236896/step/1', '236897/step/1', '236898/step/1', '236899/step/1', '236903/step/1', '236904/step/1', '236905/step/1'])
+def test_stepic(browser, links):
+    answer = math.log(int(time.time()))
+    link = 'https://stepik.org/lesson/{}'.format(links)
+    browser.get(link)
+    time.sleep(2)
+    browser.find_element_by_tag_name('textarea').send_keys(str(answer))
+    browser.find_element_by_class_name('submit-submission').click()
+    assert "Correct!", "Корректно!"
