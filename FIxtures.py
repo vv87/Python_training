42ecbb238c2c7622d9690601397be77e1752f009
vv87 <38062151+vv87@users.noreply.github.com>
2019-07-10 17:11:05 +0300

    Create FIxtures.py

---
+++ FIxtures.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*
+"""
+import pytest
+
+
+@pytest.fixture(scope="class")
+def prepare_faces():
+    print("^_^", "\n")
+    yield
+    print(":3", "\n")
+
+
+@pytest.fixture()
+def very_important_fixture():
+    print(":)", "\n")
+
+
+@pytest.fixture(autouse=True)
+def print_smiling_faces():
+    print(":-Р", "\n")
+
+
+class TestPrintSmilingFaces():
+    def test_first_smiling_faces(self, prepare_faces, very_important_fixture):
+        # какие-то проверки
+
+    def test_second_smiling_faces(self, prepare_faces):
+        # какие-то проверки"""
+
+
+import pytest
+
+@pytest.mark.xfail(strict=True)
+def test_succeed():
+    assert True
+
+
+@pytest.mark.xfail
+def test_not_succeed():
+    assert False
+
+
+@pytest.mark.skip
+def test_skipped():
+    assert False
