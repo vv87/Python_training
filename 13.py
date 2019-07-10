ea56a22a224fb5877d6631a062a35b9a12227bd2
vv87 <38062151+vv87@users.noreply.github.com>
2019-07-10 17:05:53 +0300

    Create 13.py

---
+++ 13.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+
+browser.get("http://suninjuly.github.io/wait2.html")
+
+# говорим Selenium проверять в течение 5 секунд, пока кнопка не станет кликабельной
+button = WebDriverWait(browser, 5).until(
+        EC.element_to_be_clickable((By.ID, "check"))
+    )
+button.click()
+message = browser.find_element_by_id("check_message")
+
+assert "успешно" in message.text
